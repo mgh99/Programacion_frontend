c183ca07c7418e4014638a265bd7308325053182
María González Herrero <marygonzalezherrero@gmail.com>
2022-04-10 17:49:22 +0200

    Update Pages.tsx

--- Rick_Morty_Graph_Modal/src/Componentes/Pages.tsx
+++ Rick_Morty_Graph_Modal/src/Componentes/Pages.tsx
@@ -15,48 +15,6 @@ const Pages: FC<PageProps> = ({ page, otherPage, totalPages }) => {
         return <h1>Cargando...</h1>;
     }
 
-    /*
-    // Next 1
-    //crea una constante para el boton de siguiente pagina
-    const NextPageNumber = (IN: number) => {
-        if (IN < data!.characters.info.pages) {
-            refetch({ page: page + 1 });
-        }
-    };
-
-    //Next 1 ... 7, 8
-    //se crea una constante para el boton de primera pagina
-    const FirstPageNumber = (IN: number) => {
-        if (IN > 2) {
-            return (
-                <div className="firstPageNumber">
-                    <pre className="size-first" onClick={() => { otherPage(data!.characters.info.pages) }}>1</pre>
-                    <pre>...</pre>
-                </div>
-            )
-        }
-    };
-
-    //Next 1 ... 7, 8, 9 ... 42
-    //se crea una constante para el boton de ultima pagina
-    const LastPageNumber = (IN: number) => {
-        if (IN < data!.characters.info.pages) {
-            <div className="lastPageNumber">
-                <pre>...</pre>
-                <pre onClick={() => { otherPage(data!.characters.info.pages) }}>{data?.characters.info.pages}</pre>
-            </div>
-        }
-    };
-
-    // Next 1 ... 7, 8, 9, ... 42 Prev
-    //crea una constante para el boton de pagina anterior
-    const PrevPageNumber = (IN: number) => {
-        if (IN > 1) {
-            return <pre onClick={() => { otherPage(page - 1) }} > {page - 1}</pre>
-        }
-    };*/
-
-
     if(data) {
         return(
             <div className = "content-pages">
@@ -123,39 +81,6 @@ const Pages: FC<PageProps> = ({ page, otherPage, totalPages }) => {
             </div>
         )
     }
-
-    /*if (data) {
-        return (
-            <div className="pages">
-                <button onClick={() => {
-                    if (page > 1) { //si la pagina es mayor a 1
-                        otherPage(page - 1); //se le resta 1 a la pagina
-                    }
-                }}>Prev</button>
-
-                {FirstPageNumber(page)}
-                {PrevPageNumber(page)}
-
-                <pre className="hear" onClick={() => { otherPage(page) }}>{page}</pre> 
-
-                {NextPageNumber(page)}
-                {LastPageNumber(page)}
-
-                <button onClick={() => {
-                    if (page < data!.characters.info.pages) { //si la pagina es menor que el total de paginas
-                        otherPage(page + 1); //se + 1 a la pagina
-                    }
-                }}>Next</button>
-            </div>
-        );
-
-    } else {
-        return (
-            <div>
-                <Error />
-            </div>
-        )
-    }*/
 
 }
 
